299127becd5d30643b6c492ee57b793bb6630daa
AxelBrandel <84927377+Skraggan@users.noreply.github.com>
2022-09-13 15:39:51 +0200

    Uppgift 2 klar

--- ovning_102/parametriska_kurvor.py
+++ ovning_102/parametriska_kurvor.py
@@ -3,13 +3,60 @@ import numpy as np
 from operator import add
 FONTSIZE = 24
 
-def plot_lemniskata(origo):
-    tim.color("blue")
+def plot_descartes(origo):
+    a = 250
+    tim.color("light green")
     tim.penup()
-    tim.goto(calc_function(-2, origo, "(a*np.cos(t))/(1+(np.sin(t)**2)", "(a*np.cos(t)*np.sin(t))/(1+(np.sin(t)**2))"))
+    tim.goto(calc_function(0, origo, "a*t/(1+(t**3))", "a*(t**2)/(1+(t**3))", a))
+    tim.pendown()
 
-def plot_hyperbel(origo):
-    pass
+    t = np.arange(-0.5, 75, 0.1)
+    for i in t:  # Ritar Descarts löv, övre delen + "lövet"
+        if i == t[0]:
+            tim.penup()
+        tim.goto(calc_function(i, origo, "a*t/(1+(t**3))", "a*(t**2)/(1+(t**3))", a))
+        tim.pendown()
+
+    t = np.arange(-50, -2, 0.1)
+    for i in t:  # Ritar Descarts löv, undre delen
+        if i == t[0]:
+            tim.penup()
+        tim.goto(calc_function(i, origo, "a*t/(1+(t**3))", "a*(t**2)/(1+(t**3))", a))
+        tim.pendown()
+
+def plot_asteroid(origo, a, b):
+    tim.color("yellow")
+    tim.penup()
+    tim.goto(calc_function(0, origo, "a*(np.cos(t)**3)", "b*(np.sin(t)**3)", a ,b))
+    tim.pendown()
+
+    t = np.arange(0, 2*np.pi, 0.05)
+
+    for i in t:
+        tim.goto(calc_function(i, origo, "a*(np.cos(t)**3)", "b*(np.sin(t)**3)", a, b))
+
+
+def plot_lemniskata(origo):
+    tim.color("light blue")
+    tim.penup()
+    tim.goto(calc_function(0, origo, "(a*np.cos(t))/(1+((np.sin(t)**2)))", "(a*np.cos(t)*np.sin(t))/(1+((np.sin(t)**2)))"))
+    tim.pendown()
+
+    t = np.arange(0, 2*np.pi, 0.05)
+
+    for i in t:
+        tim.goto(calc_function(i, origo, "(a*np.cos(t))/(1+(np.sin(t)**2))", "(a*np.cos(t)*np.sin(t))/(1+(np.sin(t)**2))"))
+
+def plot_hyperbel(origo, a, b):
+    tim.color("red")
+    tim.penup()
+    tim.goto(calc_function(-2.2, origo, "a*np.cosh(t)", "b*np.sinh(t)", a, b))
+    tim.pendown()
+
+    t = np.arange(-2.2, 2.2, 0.05)
+
+    for i in t:
+        tim.goto(calc_function(i, origo, "a*np.cosh(t)", "b*np.sinh(t)", a, b))
 
 def plot_parable(origo):
     """
@@ -92,5 +139,22 @@ print_text("Ellips", origo)  # Skriver texten i ellipsen
 origo = (-100,150)
 plot_parable(origo)
 print_text("Parabel", (origo[0]+100, origo[1]))
+
+origo = (100, 150)
+plot_hyperbel(origo, 60, 20)
+print_text("Hyperbel", (origo[0]+200, origo[1]))
+
+origo = (-300, -100)
+plot_lemniskata(origo)
+print_text("Lemniskata", (origo[0], origo[1]-100))
+
+origo = (0, -100)
+plot_asteroid(origo, 100, 100)
+print_text("Asteroid", (origo[0], origo[1]-150))
+
+origo = (250, -100)
+plot_descartes(origo)
+print_text("Descartes löv", (origo[0], origo[1]-100))
+
 
 screen.exitonclick()
